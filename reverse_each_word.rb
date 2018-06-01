f8cc8455ec436820701a90500a926f91ca1fbdbb
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-01 21:01:23 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(s)
   new_array = []
   s_array = s.split(" ")
   s_array.each {|w| new_array << w.reverse}
-  return new_array
+  return new_array.join(" ")
 end
 
 # def reverse_each_word(s)
